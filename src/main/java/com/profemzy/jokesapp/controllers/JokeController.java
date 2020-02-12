8561c536029fcc7e329604ec7322e5a4e4f0e326
Femi Oladele <profemzy@gmail.com>
2020-02-12 20:46:21 +0100

    Add Joke Controller and View to Display Joke

---
+++ src/main/java/com/profemzy/jokesapp/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package com.profemzy.jokesapp.controllers;
+
+import com.profemzy.jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
