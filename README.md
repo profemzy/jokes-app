04d533db4aae86391eee9fa290499c5098303edc
Femi Oladele <profemzy@gmail.com>
2020-02-12 20:13:38 +0100

    Initial Commit with get jokes interface

---
+++ README.md
@@ -0,0 +1,3 @@
+### Spring Boot Jokes App
+
+A Spring Boot web inspired by Spring Guru - JT
---
+++ src/main/java/com/profemzy/jokesapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.profemzy.jokesapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
